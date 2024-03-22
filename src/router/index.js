5f8527b21c59141e127637ea79fe1c07a7e5a739
黄昏 <3012407647@qq.com>
2024-03-22 09:52:51 +0800

    build配置和路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,33 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from '../components/Login.vue'
-import Home from '../components/Home.vue'
-import Welcome from '../components/Welcome.vue'
-import Users from '../components/user/Users.vue'
-import Rights from '../components/power/Rights.vue'
-import Cate from '../components/games/Cate.vue'
-import GameList from '../components/games/GameList.vue'
-import AddGame from '../components/games/Addgame.vue'
-import HomePage from '../views/HomePage.vue'
-import UserHomePage from '../views/UserHomePage.vue'
-import UserProfile from '../views/UserProfile.vue'
-import DetailPage from '../views/DetailPage.vue'
+
+// import Login from '../components/Login.vue'
+const Login = () => import(/* webpackChunkName: "Login_Home_Welcome" */ '../components/Login.vue')
+// import Home from '../components/Home.vue'
+const Home = () => import(/* webpackChunkName: "Login_Home_Welcome" */ '../components/Home.vue')
+// import Welcome from '../components/Welcome.vue'
+const Welcome = () => import(/* webpackChunkName: "Login_Home_Welcome" */ '../components/Welcome.vue')
+
+// import Users from '../components/user/Users.vue'
+const Users = () => import(/* webpackChunkName: "Users_Rights_Roles" */ '../components/user/Users.vue')
+// import Rights from '../components/power/Rights.vue'
+const Rights = () => import(/* webpackChunkName: "Users_Rights_Roles" */ '../components/power/Rights.vue')
+
+// import Cate from '../components/games/Cate.vue'
+const Cate = () => import(/* webpackChunkName: "Cate_Params" */ '../components/games/Cate.vue')
+// import GameList from '../components/games/GameList.vue'
+const GameList = () => import(/* webpackChunkName: "Cate_Params" */ '../components/games/GameList.vue')
+// import AddGame from '../components/games/Addgame.vue'
+const AddGame = () => import(/* webpackChunkName: "Cate_Params" */ '../components/games/Addgame.vue')
+
+// import HomePage from '../views/HomePage.vue'
+const HomePage = () => import(/* webpackChunkName: "HomePage" */ '../views/HomePage.vue')
+// import UserHomePage from '../views/UserHomePage.vue'
+const UserHomePage = () => import(/* webpackChunkName: "HomePage" */ '../views/UserHomePage.vue')
+// import UserProfile from '../views/UserProfile.vue'
+const UserProfile = () => import(/* webpackChunkName: "HomePage" */ '../views/UserProfile.vue')
+// import DetailPage from '../views/DetailPage.vue'
+const DetailPage = () => import(/* webpackChunkName: "HomePage" */ '../views/DetailPage.vue')
 
 Vue.use(VueRouter)
 
@@ -44,6 +60,7 @@ const routes = [
 ]
 
 const router = new VueRouter({
+  // mode: 'hash',
   routes
 })
 
--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: './',
   chainWebpack: congfig => {
     congfig.when(process.env.NODE_ENV === 'production', congfig => {
       congfig.entry('app').clear().add('./src/main-prod.js')
 
       congfig.set('externals', {
         vue: 'Vue',
+        'vue-router': 'VueRouter',
+        axios: 'axios',
         lodash: '_',
+        nprogress: 'NProgress',
         'vue-quill-editor': 'VueQuillEditor'
       })
 
